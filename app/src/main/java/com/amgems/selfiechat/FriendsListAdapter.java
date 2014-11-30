0494aee557e5544ee8ef4ab011f441b484e94df7
shermpay <shermanpay1991@gmail.com>
2014-11-30 00:20:34 -0800

    Added card deps. Setup basic cards

--- app/src/main/java/com/amgems/selfiechat/FriendsListAdapter.java
+++ app/src/main/java/com/amgems/selfiechat/FriendsListAdapter.java
@@ -1,10 +1,13 @@
 package com.amgems.selfiechat;
 
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -24,13 +27,13 @@ public class FriendsListAdapter extends RecyclerView.Adapter<FriendsListAdapter.
         View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.friends_list_adapter, parent, false);
         // Setup View here
-        ViewHolder vh = new ViewHolder((TextView)v);
+        ViewHolder vh = new ViewHolder((CardView)v);
         return vh;
     }
 
     @Override
     public void onBindViewHolder(FriendsListAdapter.ViewHolder viewHolder, int i) {
-        viewHolder.mTextView.setText(mFriendsList.get(i));
+        viewHolder.mFriendNameView.setText(mFriendsList.get(i));
     }
 
     @Override
@@ -39,15 +42,17 @@ public class FriendsListAdapter extends RecyclerView.Adapter<FriendsListAdapter.
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        TextView mTextView;
-        public ViewHolder(TextView itemView) {
+        CardView mFriendCard;
+        TextView mFriendNameView;
+        public ViewHolder(CardView itemView) {
             super(itemView);
-            mTextView = itemView;
+            mFriendCard = itemView;
+            mFriendNameView = (TextView)itemView.findViewById(R.id.friend_name);
         }
 
         @Override
         public String toString() {
-            return mTextView.toString();
+            return mFriendCard.toString();
         }
     }
 }
