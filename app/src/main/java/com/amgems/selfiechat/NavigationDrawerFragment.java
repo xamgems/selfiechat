77cc94a28f60aebd8055ce23a85e23e2df7fad3e
Zachary Iqbal <zachary.iq@gmail.com>
2014-11-30 19:21:44 -0800

    Added initial navigation drawer animations, avatar and cover.

--- app/src/main/java/com/amgems/selfiechat/NavigationDrawerFragment.java
+++ app/src/main/java/com/amgems/selfiechat/NavigationDrawerFragment.java
@@ -1,10 +1,19 @@
 package com.amgems.selfiechat;
 
+import android.animation.AnimatorSet;
+import android.animation.TimeInterpolator;
+import android.animation.ValueAnimator;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
+import android.graphics.BitmapFactory;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -12,6 +21,8 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewPropertyAnimator;
+import android.widget.ImageView;
 
 public class NavigationDrawerFragment extends Fragment {
 
@@ -30,13 +41,23 @@ public class NavigationDrawerFragment extends Fragment {
     /** Position of the navigation drawer's currently selected item **/
     private int mSelectedItem;
 
+    private ColorMatrix mSaturationMatrix;
+
     /** Root container view of this drawer Fragment **/
     private View mDrawerRootView;
+
+    private ImageView mUserCover;
+    private BitmapDrawable mCoverDrawable;
+
+    private ImageView mUserAvatar;
+    private RoundedBitmapDrawable mAvatarDrawable;
 
     private DrawerLayout mDrawerLayout;
 
     public NavigationDrawerFragment() {
         // Required empty public constructor
+        mSaturationMatrix = new ColorMatrix();
+        mSaturationMatrix.setSaturation(0.25f);
     }
 
     @Override
@@ -63,7 +84,31 @@ public class NavigationDrawerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+        View layout = inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+        mUserCover = (ImageView) layout.findViewById(R.id.drawer_cover);
+        mUserAvatar = (ImageView) layout.findViewById(R.id.image_user_avatar);
+        setUpAvatar(mUserAvatar, R.drawable.stub_avatar);
+        setUpCover(mUserCover, R.drawable.drawer_cover);
+        return layout;
+    }
+
+    private void setUpCover(ImageView host, int drawableResId) {
+        mCoverDrawable = new BitmapDrawable(getResources(), BitmapFactory.decodeResource(getResources(), drawableResId));
+        mCoverDrawable.setAntiAlias(true);
+        mSaturationMatrix.setSaturation(0.1f);
+        mCoverDrawable.setColorFilter(new ColorMatrixColorFilter(mSaturationMatrix));
+        host.setImageDrawable(mCoverDrawable);
+    }
+
+    //TODO(zac): Create an ImageView subclass that can preform this mask automagically
+    private void setUpAvatar(ImageView host, int drawableResId) {
+        mAvatarDrawable =
+                RoundedBitmapDrawableFactory.create(getResources(), BitmapFactory.decodeResource(getResources(), drawableResId));
+        mAvatarDrawable.setAntiAlias(true);
+        mAvatarDrawable.setCornerRadius(mAvatarDrawable.getIntrinsicWidth() / 2);
+        mSaturationMatrix.setSaturation(0.25f);
+        mAvatarDrawable.setColorFilter(new ColorMatrixColorFilter(mSaturationMatrix));
+        host.setImageDrawable(mAvatarDrawable);
     }
 
     @Override
@@ -96,6 +141,47 @@ public class NavigationDrawerFragment extends Fragment {
                     preferences.edit().putBoolean(PREF_DRAWER_LEARNED, true).apply();
                 }
 
+                TimeInterpolator fastOutLinearIn = new TimeInterpolator() {
+                    @Override
+                    public float getInterpolation(float x) {
+                        return (float) (6 * Math.pow(x, 2) - 8 * Math.pow(x, 3) + 3 * Math.pow(x, 4));
+                    }
+                };
+                // Animates saturation of avatar
+                ValueAnimator avatarSatAnim = ValueAnimator.ofFloat(0.25f, 0.95f);
+                avatarSatAnim.setDuration(800);
+                avatarSatAnim.setInterpolator(fastOutLinearIn);
+                avatarSatAnim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        float satValue = (float) animation.getAnimatedValue();
+                        mSaturationMatrix.setSaturation(satValue);
+                        mAvatarDrawable.setColorFilter(new ColorMatrixColorFilter(mSaturationMatrix));
+                    }
+                });
+
+                ValueAnimator coverSatAnim = ValueAnimator.ofFloat(0.1f, 0.60f);
+                coverSatAnim.setDuration(1000);
+                coverSatAnim.setInterpolator(fastOutLinearIn);
+                coverSatAnim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        float satValue = (float) animation.getAnimatedValue();
+                        mSaturationMatrix.setSaturation(satValue);
+                        mCoverDrawable.setColorFilter(new ColorMatrixColorFilter(mSaturationMatrix));
+                    }
+                });
+
+                AnimatorSet satAnimationSet = new AnimatorSet();
+                satAnimationSet.playTogether(avatarSatAnim, coverSatAnim);
+                satAnimationSet.start();
+
+                // Animates opacity of avatar
+                ViewPropertyAnimator opacityAnim = mUserAvatar.animate().alpha(1.0f).translationX(0);
+                opacityAnim.setDuration(700);
+                opacityAnim.setInterpolator(fastOutLinearIn);
+                opacityAnim.start();
+
                 getActivity().invalidateOptionsMenu();
             }
 
@@ -105,6 +191,13 @@ public class NavigationDrawerFragment extends Fragment {
                 if (!isAdded()) {
                     return;
                 }
+
+                // Resets state of animated views
+                mSaturationMatrix.setSaturation(0.25f);
+                mAvatarDrawable.setColorFilter(new ColorMatrixColorFilter(mSaturationMatrix));
+                mSaturationMatrix.setSaturation(0.1f);
+                mCoverDrawable.setColorFilter(new ColorMatrixColorFilter(mSaturationMatrix));
+                mUserAvatar.setTranslationX(-mUserAvatar.getRight());
 
                 getActivity().invalidateOptionsMenu();
             }
