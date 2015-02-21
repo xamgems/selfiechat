df6aa0e1cdb5c98190729df5f49e3481d77d1aaf
shermpay <shermanpay1991@gmail.com>
2015-02-21 13:40:56 -0800

    Added CameraFragment and FragmentPager

--- app/src/main/java/com/amgems/selfiechat/HomeActivity.java
+++ app/src/main/java/com/amgems/selfiechat/HomeActivity.java
@@ -1,11 +1,17 @@
 package com.amgems.selfiechat;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.amgems.selfiechat.view.CameraFragment;
 
 
 public class HomeActivity extends ActionBarActivity {
@@ -14,6 +20,12 @@ public class HomeActivity extends ActionBarActivity {
     private DrawerLayout mDrawerLayout;
     /** Fragment that manages the navigation drawer **/
     private NavigationDrawerFragment mDrawerFragment;
+    /** Friends List fragment **/
+    private FriendsListFragment mFriendsFragment;
+    /** Camera fragment **/
+    private CameraFragment mCameraFragment;
+    /** Pager to page fragments **/
+    private ViewPager mPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,16 +40,25 @@ public class HomeActivity extends ActionBarActivity {
 
     public void setUp() {
         mDrawerLayout = (DrawerLayout) findViewById(R.id.home_drawer_layout);
+        FragmentManager fragmentManager = getSupportFragmentManager();
         mDrawerFragment = (NavigationDrawerFragment)
-                getSupportFragmentManager().findFragmentById(R.id.navigation_fragment);
+                fragmentManager.findFragmentById(R.id.navigation_fragment);
         mDrawerFragment.setUp(R.id.navigation_fragment, mDrawerLayout, getSupportActionBar());
+
+        mPager = (ViewPager) findViewById(R.id.fragment_pager);
+        mPager.setAdapter(new FragmentPager(fragmentManager));
+        mFriendsFragment = (FriendsListFragment)
+                fragmentManager.findFragmentById(R.id.friends_list_fragment);
+
+        mCameraFragment = (CameraFragment)
+                fragmentManager.findFragmentById(R.id.camera_fragment);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_home, menu);
-        setTitle(R.string.frag_friends_title);
+        setTitle(R.string.app_name);
         return true;
     }
 
@@ -54,5 +75,32 @@ public class HomeActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public static class FragmentPager extends FragmentPagerAdapter {
+        private static int TAB_COUNT = 2;
+        private Fragment[] fragments;
+
+        public FragmentPager(FragmentManager fm) {
+            super(fm);
+            fragments = new Fragment[TAB_COUNT];
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            if (fragments[position] == null) {
+                if (position == 0) {
+                    fragments[0] = CameraFragment.newInstance();
+                } else if (position == 1) {
+                    fragments[1] = FriendsListFragment.newInstance();
+                }
+            }
+            return fragments[position];
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.length;
+        }
     }
 }
---
+++ app/src/main/java/com/amgems/selfiechat/view/CameraFragment.java
@@ -0,0 +1,47 @@
+package com.amgems.selfiechat.view;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.amgems.selfiechat.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link CameraFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class CameraFragment extends Fragment {
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment CameraFragment.
+     */
+    public static CameraFragment newInstance() {
+        CameraFragment fragment = new CameraFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public CameraFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_camera, container, false);
+    }
+}
